664373ebf78e24f674da14ba56bd79b3de7e2fb1
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-09-17 12:39:41 +0600

    product and price show

--- components/shared/product/ProductCart.tsx
+++ components/shared/product/ProductCart.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import ProductPrice from './ProductPrice';
 // import ProductPrice from './product-price';
 // import { Product } from '@/types';
 // import Rating from './rating';
@@ -24,14 +25,14 @@ const ProductCard = ({ product }: { product:any }) => {
         <Link href={`/product/${product.slug}`}>
           <h2 className='text-sm font-medium'>{product.name}</h2>
         </Link>
-        {/* <div className='flex-between gap-4'>
-          <Rating value={Number(product.rating)} />
+        <div className='flex-between gap-4'>
+          {/* <Rating value={Number(product.rating)} /> */}
           {product.stock > 0 ? (
             <ProductPrice value={Number(product.price)} />
           ) : (
             <p className='text-destructive'>Out Of Stock</p>
           )}
-        </div> */}
+        </div>
       </CardContent>
     </Card>
   );
